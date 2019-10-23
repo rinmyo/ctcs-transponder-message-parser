34f0b3e32ceb97446329dd304aab17bbc6cbd1c1
Satanya <admin@sozr.cn>
2019-10-23 14:14:58 +0800

    add more

--- Message.go
+++ Message.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"strconv"
 )
@@ -23,12 +23,12 @@ func (msg BinMessage) Decode2FrameMark() (result FrameMark) {
 	varLengthMap := make(map[string]int) //fm爲長度map
 	result = make(FrameMark)
 
-	b, err := ioutil.ReadFile("./packets/FRAMEMARK.json")
+	b, err := ioutil.ReadFile("./packets/FRAMEMARK.yml")
 	if err != nil {
 		fmt.Print(err)
 	}
 
-	err = json.Unmarshal([]byte(b), &varLengthMap)
+	err = yaml.Unmarshal([]byte(b), &varLengthMap)
 	if err != nil {
 		panic(err)
 	}
@@ -42,53 +42,82 @@ func (msg BinMessage) Decode2FrameMark() (result FrameMark) {
 	return
 }
 
-func Decode2EtcsPacket(binStr string) (result UserInfoPacket) {
-	varLengthMap := make(map[string]interface{})
-	result = make(UserInfoPacket)
+//解析單個包
+func Decode2EtcsPacket(binStr string) (result []UserInfoPacket) {
+	varLengthMap := make(map[interface{}]interface{})
+	result = make([]UserInfoPacket, 0)
 
-	b, err := ioutil.ReadFile("./packets/ETCS-" + strconv.Itoa(BIN2DEC(binStr[0:8])) + ".json")
+	b, err := ioutil.ReadFile("./packets/ETCS-" + strconv.Itoa(BIN2DEC(binStr[0:8])) + ".yml")
 	if err != nil {
 		fmt.Print(err)
 	}
 
-	err = json.Unmarshal([]byte(b), &varLengthMap)
+	err = yaml.Unmarshal([]byte(b), &varLengthMap)
 	if err != nil {
 		panic(err)
 	}
 
 	//將全部視爲一個長度爲一的循環體
-	parseUnfixedPart(1, binStr, varLengthMap)
+	for end := 0; end < 772; {
+		if binStr[end:end+8] == "11111111" {
+			fmt.Println("解析結束")
+		}
+		packetLength := BIN2DEC(binStr[end+10 : end+23])
+		fixedPart := parseUnfixedPart(1, binStr[end:end+packetLength], varLengthMap)[0]
+		result = append(result, fixedPart)
+		end += packetLength
+	}
 
+	fmt.Println("報文有誤")
 	return
 }
 
 /**
+* 僅僅解析單個用戶信息包
 length 長度 bin 待解析的二進制字符串 format 需要解析至的格式，string爲變量名， int爲該變量的長度
 */
-func parseUnfixedPart(length int, bin string, varLengthMap map[string]interface{}) []map[string]interface{} {
+func parseUnfixedPart(length int, bin string, varLengthMap map[interface{}]interface{}) []map[string]interface{} {
 	result := make([]map[string]interface{}, length)
 
 	end := 0 //尾指針
 
 	for i := 0; i < length; i++ {
 		result[i] = make(map[string]interface{})
+		// 該循環僅給value爲整數複製
 		for k, v := range varLengthMap {
-			// value 是當前部分的長度 或 嵌套的部分
-			if value, ok := v.(float64); ok {
-				// 如果是數字的話
-				result[i][k] = BIN2DEC(bin[end : end+int(value)])
-				end += int(value)
-				//遇到iter跳過下一個，因爲下一個是數組
-				if k[0:6] == "N_ITER" {
-					arr := k[7:] + "_ARRAY"
-					result[i][arr] = parseUnfixedPart(int(value), bin[end:], varLengthMap[arr].(map[string]interface{}))
-					continue
+			// value 是當前部分的長度
+			if value, ok := v.(int); ok {
 
+				// 如果是數字的話
+				result[i][k.(string)] = BIN2DEC(bin[end : end+int(value)])
+				end += int(value)
+
+				if len(k.(string)) == 8 && k.(string)[0:6] == "N_ITER" {
+					arr := k.(string)[7:] + "_ARRAY"
+					result[i][arr] = parseUnfixedPart(int(value), bin[end:], varLengthMap[arr].(map[interface{}]interface{}))
 				}
 			}
+		}
+
+		//若是etcs44單獨處理
+		if result[i]["NID_PACKET"] == 44 {
+
+			b, err := ioutil.ReadFile("./packets/CTCS-" + strconv.Itoa(result[i]["NID_XUSER"].(int)) + ".yml") //打開對應的yml檔案
+			if err != nil {
+				fmt.Print(err)
+			}
+			//解析到varLengthMap中
+			err = yaml.Unmarshal([]byte(b), &varLengthMap)
+			if err != nil {
+				panic(err)
+			}
+
+			result[i]["XXXXXX"] = parseUnfixedPart(1, bin[23:result[i]["L_PACKET"].(int)], varLengthMap)[0]
 
 		}
+
 	}
+
 	return result
 }
 
@@ -100,7 +129,7 @@ type UserInfoPacket map[string]interface{}
 
 //解包到json字符串
 func (p UserInfoPacket) Parse2json() (result string) {
-	data, err := json.Marshal(map[string]interface{}(p))
+	data, err := yaml.Marshal(map[string]interface{}(p))
 	if err != nil {
 		panic(err)
 	}
